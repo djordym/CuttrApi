91129aecc70f9645921e90f903015dabe5ed10cd
djordym <131238624+djordym@users.noreply.github.com>
2025-02-13 14:40:41 +0100

    dd

--- Cuttr.Business/Contracts/Outputs/PlantResponse.cs
+++ Cuttr.Business/Contracts/Outputs/PlantResponse.cs
@@ -14,7 +14,7 @@ namespace Cuttr.Business.Contracts.Outputs
         public string SpeciesName { get; set; }
         public string? Description { get; set; }
 
-        public PlantStage PlantStage { get; set; }
+        public PlantStage? PlantStage { get; set; }
         public PlantCategory? PlantCategory { get; set; }
         public WateringNeed? WateringNeed { get; set; }
         public LightRequirement? LightRequirement { get; set; }
